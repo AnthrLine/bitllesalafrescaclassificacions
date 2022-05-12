703b8ba46dad774a1aa08e45df586d8f7255ef21
Anthr. Line <64476467+AnthrLine@users.noreply.github.com>
2022-05-12 21:22:29 +0200

    Adding folders with the need to remove bad js

---
+++ static/js/script.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function () {
+	const equipsfederats = document.getElementById("equipsfederats")
+	const equipsnofederats = document.getElementById("equipsnofederats")
+	const individualnofederats = document.getElementById("individualnofederats")
+	const individualfederats = document.getElementById("individualfederats")
+	const individual20102014 = document.getElementById("individual20102014")
+	const individual20062009 = document.getElementById("individual20062009")
+
+	equipsfederats.onclick = function () {
+		window.location.replace("http://bitllesalafresca.anthrline.repl.co/equipsfederats");
+	}
+
+	equipsnofederats.onclick = function () {
+		window.location.replace("http://bitllesalafresca.anthrline.repl.co/equipsnofederats");
+	}	
+
+	individualnofederats.onclick = function () {
+		window.location.replace("http://bitllesalafresca.anthrline.repl.co/individualnofederats");
+	}
+
+	individualfederats.onclick = function () {
+		window.location.replace("http://bitllesalafresca.anthrline.repl.co/individualfederats");
+	}
+
+	individual20102014.onclick = function () {
+		window.location.replace("http://bitllesalafresca.anthrline.repl.co/individual20102014");
+	}
+
+	individual20062009.onclick = function () {
+		window.location.replace("http://bitllesalafresca.anthrline.repl.co/individual20062009");
+	}
+
+	
+})
---
+++ static/js/tomenu.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function () {
+	const tomenu = document.getElementById("tomenu")
+	
+	tomenu.onclick = function () {
+		window.location.replace("http://bitllesalafresca.anthrline.repl.co");
+	}
+})
